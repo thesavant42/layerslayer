d66b49ad3ee9806e7aed51c10f41a81a678aedbf
Josh Brashars <jbrashars@gmail.com>
2026-01-14 16:42:42 -0800

    fixing storage location

--- app/modules/keepers/storage.py
+++ app/modules/keepers/storage.py
@@ -18,7 +18,7 @@ from app.modules.formatters import parse_image_ref
 # Database Configuration
 # =============================================================================
 
-DEFAULT_DB_PATH = "data/lsng.db"
+DEFAULT_DB_PATH = "app/data/lsng.db"
 DEFAULT_JSON_DIR = "loot"
 
 
